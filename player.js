da86ad6eb07d76e2de59d53982204d2cd5de7536
Adel Lassag <adetech.aadl@gmail.com>
2019-06-19 19:24:44 +0100

    fix: change collide behaviour

--- player.js
+++ player.js
@@ -20,11 +20,15 @@ class Player {
   }
 
   hits(ship) {
+    /* let xp = this.x + this.r * 0.5;
+    let yp = this.x + this.r * 0.5;
+    let xs = ship.y + ship.r * 0.5;
+    let ys = ship.y + ship.r * 0.5;
+    return collideCircleCircle(xp, yp, this.r, xs, ys, ship.r); */
     return collideRectRect(this.x, this.y, this.r, this.r, ship.x-5, ship.y, ship.r, ship.r);
   }
 
   show() {
     image(pImg, this.x, this.y, this.r, this.r);
-    
   }
 }
