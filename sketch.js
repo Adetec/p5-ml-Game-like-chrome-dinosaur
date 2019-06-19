ecfacf44e83436080640ce6b0300e943818e9fd5
Adel Lassag <adetech.aadl@gmail.com>
2019-06-19 18:36:58 +0100

    feat: Add sound classifier feature

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-let player, pImg, sImg, bImg;
+let player, pImg, sImg, bImg, soundClassifier;
 let ships = []
 
 function preload() {
+  const options = { probabilityThreshold: 0.95 };
+  soundClassifier = ml5.soundClassifier('speechCommands18w', options)
   pImg = loadImage('player.png')  
   bImg = loadImage('background.png')  
   sImg = loadImage('ship.png')  
@@ -9,14 +11,25 @@ function preload() {
 
 
 function setup() {
+  createCanvas(800,450);
   player = new Player();
   ship = new Ship();
-  createCanvas(800,450);
+  soundClassifier.classify(gotCommand);
+}
+
+function gotCommand(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  if (results[0].label == 'up') {
+    player.jump();
+    console.log(results[0].label);
+  }
 }
 
 function keyPressed() {
   if (key == ' ') {
-    player.jump()
+    player.jump();
   }
 }
 
